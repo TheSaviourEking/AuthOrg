fe7b4f891326df1b9ffe489872cfe539c22ab847
TheSaviourEking <saviour.eking@outlook.com>
2024-07-08 22:51:41 +0100

    chore: add middleware for authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,6 @@
 const { verifyToken } = require('../utils/jwt');
 const { User } = require('../db/models');
 
-const authenticate = (req, res, next) => {
-    // const token = req.headers.authorization?.split(' ')[1];
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: 'Access token is missing or invalid' });
-    }
-
-    const decoded = verifyToken(token);
-    console.log(decoded)
-    if (!decoded) {
-        return res.status(401).json({ message: 'Invalid or expired token' });
-    }
-
-    req.user = decoded; // Attach decoded user info to request object
-    next();
-};
-
 const authenticateUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1]; // Get token from Authorization header
 
@@ -27,8 +10,14 @@ const authenticateUser = async (req, res, next) => {
 
     try {
         const decodedUser = verifyToken(token);
-        req.user = await User.findByPk(decodedUser.id); // Attach user object to request
-        next();
+        if (decodedUser) {
+            req.user = await User.findByPk(decodedUser.userId); // Attach user object to request
+            next();
+        }
+        else {
+            const err = new Error('invalid user')
+            next(err)
+        }
     } catch (error) {
         console.error('Token verification error:', error);
         return res.status(401).json({ error: 'Unauthorized: Invalid token' });
@@ -36,4 +25,4 @@ const authenticateUser = async (req, res, next) => {
 };
 
 
-module.exports = { authenticate, authenticateUser };
+module.exports = { authenticateUser };
